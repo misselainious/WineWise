86e50ea2a06d5b9cd5fd6cb889cb9a6848112cad
Elaine <elaine.cadman@gmail.com>
2019-09-02 22:56:57 -0700

    no fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 
 // Connect to the Mongo DB
 
-mongoose.connect('mongodb+srv://elaine:SfCa6QtpKmP4NHW0@cluster0-j8x1r.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/wineDB', { useNewUrlParser: true });
 mongoose.connection.on("open", function (ref) {
   console.log("Connected to mongo server.");
 });
