ba52884d6313a614007a56de27a308e9273f5439
Elaine <elaine.cadman@gmail.com>
2019-09-02 22:34:21 -0700

    atlas fix

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://elaine:SfCa6QtpKmP4NHW0@cluster0-j8x1r.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});;
+const mongoose = require("mongoose");
 const routes = require("./routes");
 
 // Connect to the Mongo DB
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/wineDB', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://elaine:SfCa6QtpKmP4NHW0@cluster0-j8x1r.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 mongoose.connection.on("open", function (ref) {
   console.log("Connected to mongo server.");
 });
