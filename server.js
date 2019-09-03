a543407e2456879ee405f0bc8bccbd848faa7c9e
Elaine <elaine.cadman@gmail.com>
2019-09-02 22:22:03 -0700

    new atlas approach

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://elaine:SfCa6QtpKmP4NHW0@cluster0-j8x1r.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});;
 const routes = require("./routes");
 
 // Connect to the Mongo DB
