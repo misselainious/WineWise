d09c75f16079c861ed1e4d9bd598a425fa9b119c
Elaine <elaine.cadman@gmail.com>
2019-10-02 09:26:26 -0700

    aldinger

--- client/src/components/Event/event.js
+++ client/src/components/Event/event.js
@@ -1,26 +1,21 @@
 import React, { Component } from "react";
 import { Button, Header, Icon, Segment, Advertisement, Image } from 'semantic-ui-react'
+import { Link } from "react-router-dom";
 import './event.css';
 
 const Event = () => (
     <div style= {{ marginTop: '100px'}}>
     <Segment inverted color='grey' attached ='top'> 
-        <Header  className='banner'>
-        Selections for 
-        <Header target="_blank" href='https://www.mastersofwine.org' className='banner' style= {{ display: 'inline'}}> Masters of Wine </Header>
-        Champagne Tasting
-        </Header>
+    <Link to="/producerdetails/Aldinger" >
+        <Header className='toplinked'  style= {{ display: 'inline'}}>WineWise Welcomes Aldinger! </Header>
+        </Link>
     </Segment>
+
     <Segment attached className='chosenProducers'>
-        <Header className='linked' href='https://www.winewise.biz/producerdetails/J%C3%A9r%C3%B4me%20Coessens' style= {{ display: 'inline'}}>Jérôme Coessens, </Header>
-        <Header className='linked' href='https://www.winewise.biz/producerdetails/Dehours%20&%20Fils' style= {{ display: 'inline'}}>Dehours & Fils, </Header>
-        <Header className='linked' href='https://www.winewise.biz/producerdetails/Diebolt-Vallois' style= {{ display: 'inline'}}>Diebolt-Vallois</Header>
-          
-        <Image src="../images/group/MOW.png" size='large' centered style={{ marginTop: '20px'}}/>
+        <Image src="../images/group/GAL.png" size='large' centered style={{ marginTop: '20px'}}/>
     </Segment>
     <Segment className="bottom" attached='bottom'>
-        We are pleased to announce three of our Champagne Producers will be featured 
-        in this prestigious tasting.
+        The newest addtion to our German portfolio
     </Segment>
   </div>
 )
